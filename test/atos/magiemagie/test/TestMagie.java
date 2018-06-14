d987d237e9a08746bfcbe54900b0e33591a67c50
Administrateur <Administrateur@GSVH.formation.local>
2018-06-14 11:39:01 +0200

    Entités sont mappées

---
+++ test/atos/magiemagie/test/TestMagie.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atos.magiemagie.test;
+
+import javax.persistence.Persistence;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class TestMagie {
+    
+    @Test
+    public void demarrerJpa(){
+        Persistence.createEntityManagerFactory("MagieMagiePU");
+    }
+    
+}
